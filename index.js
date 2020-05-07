d08fca8d2b967537338e72bc397c75161d2646f8
Elymas-Magus <41921067+Elymas-Magus@users.noreply.github.com>
2020-05-07 06:32:56 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const url = "https://api.sheety.co/30b6e400-9023-4a15-8e6c-16aa4e3b1e72"
 
+// Bea json() é um methodo do jQuery, dps teste com JSON.parse.
+// O metodo json() também não é suportado por alguns browsers
+// como IE, firefox android, Samsung Internet e Android Webview
 fetch(url)
   .then(response => response.json())
   .then(data => {
